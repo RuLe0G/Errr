11271165a81cf43f793be21b0d8d0c6d67530fea
RuLe0G <khlebnikov2015@yandex.ru>
2019-03-06 09:27:35 +0700

    second

--- Errr/Errr/MainWindow.xaml.cs
+++ Errr/Errr/MainWindow.xaml.cs
@@ -24,5 +24,71 @@ namespace Errr
         {
             InitializeComponent();
         }
+
+
+        public bool test(int n)
+        {
+            if (n < 1) throw new ArgumentException ("Число должно быть > 1");
+
+                for (int i = 2; i < n; i++)
+                    if (n % i == 0) return false;
+
+            return true;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool a = test(int.Parse(tb.Text));
+
+
+                if (a == false) MessageBox.Show("Число не простое");//throw new ArgumentException("Число не простое");
+                else inp.Items.Add(int.Parse(tb.Text));
+                tb.Text = "";
+            }
+
+            catch (FormatException)
+            {
+                lb.Content = "не корректный ввод: вводится не число";
+            }
+
+            catch (ArgumentException ex) //реакция на неверный аргумент
+            {
+                lb.Content = ex.Message;
+            }
+
+        }
+
+
+        private void load_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            dlg.ShowDialog();
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(dlg.FileName);
+
+            while ((line = file.ReadLine()) != null)
+            {
+
+            }
+            file.Close();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            dlg.ShowDialog();
+
+
+        }
     }
 }
